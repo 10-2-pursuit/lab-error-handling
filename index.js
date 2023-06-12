1be19327f28f4faa38fd7cfbbcc50fa60431e06c
Nicole Slater <nicoleslater@pursuit.org>
2023-06-12 01:11:20 -0400

    Submission with Genesis

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if (cart.length === 0){
+    throw Error("The Cart is Empty");
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -36,24 +39,61 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
-  const result = [];
-  for (let product of products) {
-    if (product.priceInCents >= min && product.priceInCents <= max) {
-      result.push(product);
+  // const result = [];
+  // for (let product of products) {
+  //   if (product.priceInCents >= min && product.priceInCents <= max) {
+  //     result.push(product);
+  //   }
+      if (products.length === 0) {
+        throw 'Array must not be empty';
+      }
+    
+      if (typeof min !== 'number' || typeof max !== 'number') {
+        throw 'Min and Max must be numbers';
+      }
+    
+      if (max === 0) {
+        throw 'Max must not be equal to 0';
+      }
+    
+      if (min > max) {
+        throw 'Min must not be greater than max';
+      }
+    
+      if (min < 0 || max < 0) {
+        throw 'Min and Max must be greater than 0';
+      }
+    
+      for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        if (!product.hasOwnProperty('priceInCents')) {
+          throw 'All products must have a priceInCents attribute';
     }
   }
   return result;
 }
 
+
 /*
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
+  const filteredProducts = products.filter((product) => {
+    return product.priceInCents >= min && product.priceInCents <= max;
+  });
 
-  return total;
+  return filteredProducts;
 }
+
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
+
 
 module.exports = {
   getCartTotal,
